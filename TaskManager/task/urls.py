d05874109a3c6f23af7e7d8c2d891d288291fc64
brunfrag <brfraga99@gmail.com>
2024-04-30 12:55:24 +0200

    feat(): add nuew endpoint taskLastSevenDays

--- TaskManager/task/urls.py
+++ TaskManager/task/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 
-from .viewsets import TaskViewSet
+from .viewsets import TaskViewSet, TasksLastSevenDaysViewSet
 
 router = routers.SimpleRouter();
 router.register('tasks', TaskViewSet);
+router.register('tasksLastSevenDays', TasksLastSevenDaysViewSet, 'tasks_last_seven_days');
+
 urlpatterns = router.urls
--- TaskManager/task/viewsets.py
+++ TaskManager/task/viewsets.py
@@ -1,6 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Task
+from rest_framework.response import Response
+from datetime import datetime, timedelta
 from .serializer import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all();
     serializer_class = TaskSerializer
+
+class TasksLastSevenDaysViewSet(viewsets.ViewSet):
+    def list(self, request):
+        seven_days_ago = datetime.now() - timedelta(days=7)
+        tasks = Task.objects.filter(deadline__gte=seven_days_ago)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
