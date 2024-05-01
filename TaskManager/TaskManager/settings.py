2bb820e979fb7d732b9d7f7ce6c1c33833129fce
brunfrag <brfraga99@gmail.com>
2024-05-01 15:56:16 +0200

    fix(): solucionando el problema de cors implemnatdo una librería específica

--- TaskManager/TaskManager/settings.py
+++ TaskManager/TaskManager/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = 'django-insecure-*_x3s$*w-s0xoyfei+6l-srgxb*#&z#1o^)tskfw_bnk4!s3*7
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST =(
+    'http://localhost:5173',
+)
 
 
 # Application definition
@@ -38,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'TaskManager.urls'
