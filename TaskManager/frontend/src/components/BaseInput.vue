bc98f3969c9893b32c0ff77b0c420bebc863970f
brunfrag <brfraga99@gmail.com>
2024-05-01 18:59:37 +0200

    feat(): inputs reutilizables

---
+++ TaskManager/frontend/src/components/BaseInput.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+    <div class="form-group row">
+      <label class="col-sm-2 col-form-label">{{ label }}</label>
+      <div class="col-sm-10">
+        <input  
+        addClass="form-control"
+          :class="{ 'form-control ': addClass }"
+          :value="modelValue" 
+          @input="$emit('update:modelValue', $event.target.value)"
+          v-bind="$attrs"
+        >
+      </div>
+    </div>
+
+</template>
+
+<script setup>
+  defineProps({
+    label: {
+          type: String,
+          default: ""
+    } ,
+    addClass: {
+          type: [Boolean]
+    } ,
+    modelValue: {
+    type: String,
+      default: ""
+    },
+  })
+
+</script>
+
+<style scoped>
+
+</style>
--- TaskManager/frontend/src/components/Task/TaskForm.vue
+++ TaskManager/frontend/src/components/Task/TaskForm.vue
@@ -1,18 +1,26 @@
 <script setup>
-
+import BaseInput from "../BaseInput.vue";
+import BaseSelect from "../BaseSelect.vue";
+import { ref } from 'vue';
 import axios from 'axios';
 const path = "http://localhost:8000/api/v1.0/tasks/";
+const optionsArray = [
+  { value: 'Personal', text: 'Personal' },
+  { value: 'Trabajo', text: 'Trabajo' },
+  { value: 'Estudio', text: 'Estudio' },
+  { value: 'Otros', text: 'Otros' }
+];
+
+const title = ref("");
+const description = ref("");
+const category = ref("");
+const deadline = ref("");
+const completed = ref("");
+
 const handleSubmit = async (event) => {
   event.preventDefault(); // Evitar el envío automático del formulario
 
-  // Acceder a los valores de los campos del formulario
-  const formData = new FormData(event.target);
-  const title = formData.get('title');
-  const description = formData.get('description');
-  const category = formData.get('category');
-  const deadline = formData.get('deadline');
-  const completed = formData.get('completed') === 'true'; // Convertir a booleano
-
+  
   try {
     // Enviar los datos a través de Axios en una solicitud POST
     const response = await axios.post(path, {
@@ -36,45 +44,11 @@ const handleSubmit = async (event) => {
     <h1>Create task form</h1>
     <form  @submit.prevent="handleSubmit" enctype="multipart/form-data" class="form-horizontal">
   <fieldset>
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Title</label>
-      <div class="col-sm-10">
-        <input name="title" class="form-control" type="text" value="">
-      </div>
-    </div>
-
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Description</label>
-      <div class="col-sm-10">
-        <textarea name="description" class="form-control"></textarea>
-      </div>
-    </div>
-
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Category</label>
-      <div class="col-sm-10">
-        <select class="form-control" name="category">
-          <option value="Personal">Personal</option>
-          <option value="Trabajo">Trabajo</option>
-          <option value="Estudio">Estudio</option>
-          <option value="Otros">Otros</option>
-        </select>
-      </div>
-    </div>
-
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Deadline</label>
-      <div class="col-sm-10">
-        <input name="deadline" class="form-control" type="datetime-local" value="">
-      </div>
-    </div>
-
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Completed</label>
-      <div class="col-sm-10">
-        <input type="checkbox" name="completed" value="true">
-      </div>
-    </div>
+    <BaseInput v-model="title"  type="text" label="Title" name="Title" addClass="form-control"/>
+    <BaseInput v-model="description"  type="text" label="Description" name="Description" addClass="form-control"/>
+    <BaseSelect v-model="category"  label="Options" name="Description" addClass="form-control" :options="optionsArray"/>
+    <BaseInput v-model="deadline"  type="datetime-local" label="Deadline" name="deadline"         addClass="form-control"/>
+    <BaseInput v-model="completed"  type="checkbox" label="Completed" name="completed"         />
 
     <div class="form-group row">
       <div class="col-sm-10 offset-sm-2">
