9c1a694f29d152a62b611aaa55c6f010cbd323e9
brunfrag <brfraga99@gmail.com>
2024-05-02 16:21:26 +0200

    feat(): cambio en el filtrado de las fechas en el endpoint de en los úlitmos siete días

--- TaskManager/task/viewsets.py
+++ TaskManager/task/viewsets.py
@@ -10,6 +10,6 @@ class TaskViewSet(viewsets.ModelViewSet):
 class TasksLastSevenDaysViewSet(viewsets.ViewSet):
     def list(self, request):
         seven_days_ago = datetime.now() - timedelta(days=7)
-        tasks = Task.objects.filter(deadline__gte=seven_days_ago)
+        tasks = Task.objects.filter(deadline__gte=seven_days_ago, deadline__lte=datetime.now())
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
