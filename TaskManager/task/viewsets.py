c093ee880f19ddce649bee5eba8dc2292fbc24fb
brunfrag <brfraga99@gmail.com>
2024-05-02 16:56:15 +0200

    feat(): agregamos las ocndición de que completed sea igual a true en el endpoint de los úlitmos site días

--- TaskManager/task/viewsets.py
+++ TaskManager/task/viewsets.py
@@ -3,13 +3,17 @@ from .models import Task
 from rest_framework.response import Response
 from datetime import datetime, timedelta
 from .serializer import TaskSerializer
+
+
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all();
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class TasksLastSevenDaysViewSet(viewsets.ViewSet):
     def list(self, request):
         seven_days_ago = datetime.now() - timedelta(days=7)
-        tasks = Task.objects.filter(deadline__gte=seven_days_ago, deadline__lte=datetime.now())
+        tasks = Task.objects.filter(
+            deadline__gte=seven_days_ago, deadline__lte=datetime.now(), completed=True)
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
